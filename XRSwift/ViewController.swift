a61d6242ba1be2d02653c3d96fbec93c56a6a302
MissSunday <963217127@qq.com>
2022-01-11 13:56:15 +0800

    增加一个view

--- XRSwift/ViewController.swift
+++ XRSwift/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var someView : UIView
+        
+        someView = UIView.init(frame: CGRect(x: 100,y: 100,width: 100,height: 100))
+        someView.backgroundColor = .red
+        self.view.addSubview(someView)
+        
+        
     }
 
 
